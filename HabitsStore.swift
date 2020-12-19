3aaf0313ebebf55b0cfdf837a09cc13bdde67415
Artem Novichkov <novichkoff93@gmail.com>
2020-12-19 16:45:47 +0600

    Update HabitsStore.swift

--- HabitsStore.swift
+++ HabitsStore.swift
@@ -124,6 +124,7 @@ public final class HabitsStore {
     
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
+        formatter.locale = .init(identifier: "ru_RU")
         formatter.dateStyle = .medium
         formatter.timeStyle = .none
         formatter.doesRelativeDateFormatting = true
